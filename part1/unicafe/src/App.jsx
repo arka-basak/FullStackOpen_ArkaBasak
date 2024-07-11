169707950ed90b8441dda56d7dd752a1b82d5e70
Arka Basak <basakarka3@gmail.com>
2024-07-10 20:34:47 -0700

    Ex1.10 Submission

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,31 +1,32 @@
 import { useState } from 'react'
 
-const FeedbackGiver = ({setGood, setNeutral, setBad, good, neutral, bad}) =>{
+
+
+const Button = ({incrementFeedback, feedback, text}) =>{
   return (
-    <>
-      <h1>give feedback</h1>
-      <button onClick = {()=> setGood(good + 1)}>good</button>
-      <button onClick = {()=> setNeutral(neutral + 1)}>neutral</button>
-      <button onClick = {()=> setBad(bad + 1)}>bad</button>
-    </>
-    
+    <button onClick = {() => incrementFeedback(feedback + 1)}>{text}</button>
   )
 }
+
+const StatisticLine = ({text, value}) =>{
+  return <div>{text} {value}</div>
+}
+
 
 const Statistics = ({good,neutral,bad}) => {
   const all = good + bad + neutral
   if (all > 0){
     const average = (good - bad)/all 
-    const positive = (good/all) * 100 
+    const positive = String((good/all) * 100 ) + " %"
     return (
         <>
           <h1>statistics</h1>
-          <div>good {good}</div>
-          <div>neutral {neutral}</div>
-          <div>bad {bad}</div>
-          <div>all {all}</div>
-          <div>average {average}</div>
-          <div>positive {positive} %</div>
+          <StatisticLine text = 'good' value = {good}></StatisticLine>
+          <StatisticLine text = 'neutral' value = {neutral}></StatisticLine>
+          <StatisticLine text = 'bad' value = {bad}></StatisticLine>
+          <StatisticLine text = 'all' value = {all}></StatisticLine>
+          <StatisticLine text = 'average' value = {average}></StatisticLine>
+          <StatisticLine text = 'positive' value = {positive}></StatisticLine>
         </>
     )
   }else{
@@ -47,7 +48,10 @@ const App = () => {
 
   return (
     <>
-      <FeedbackGiver setGood = {setGood} setNeutral = {setNeutral} setBad = {setBad} good = {good} neutral = {neutral} bad = {bad}/>
+      <h1>give feedback</h1>
+      <Button incrementFeedback={setGood} feedback = {good} text = {'good'}/>
+      <Button incrementFeedback={setNeutral} feedback = {neutral} text = {'neutral'}/>
+      <Button incrementFeedback={setBad} feedback = {bad} text = {'bad'}/>
       <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </>
   )
